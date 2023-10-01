eaeddc00e5e7d76217acec0fe1584a6298aa1e82
happy5happy5 <zombil8731@gmail.com>
2023-10-01 19:50:10 +0900

    first commit

---
+++ src/main/java/com/demo1010/researcherv2/ResearcherV2Application.java
@@ -0,0 +1,13 @@
+package com.demo1010.researcherv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResearcherV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResearcherV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo1010/researcherv2/ResearcherV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo1010.researcherv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResearcherV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
