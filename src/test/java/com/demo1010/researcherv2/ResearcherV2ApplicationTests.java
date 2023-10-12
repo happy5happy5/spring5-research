39c7f56ef92747fe07c64788380573f2c28b4b6f
happy5happy5 <zombil8731@gmail.com>
2023-10-12 17:30:05 +0900

    save

--- src/test/java/com/demo1010/researcherv2/ResearcherV2ApplicationTests.java
+++ src/test/java/com/demo1010/researcherv2/ResearcherV2ApplicationTests.java
@@ -3,7 +3,7 @@ package com.demo1010.researcherv2;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ResearcherV2ApplicationTests {
 
     @Test
